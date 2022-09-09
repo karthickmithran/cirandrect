a17a4bfed846995cc3c5ce41dc86cab1605469a0
karthickmithran <90264421+karthickmithran@users.noreply.github.com>
2022-09-09 22:49:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-function preload() {
-}
-
-function setup() {  
-canvas = createCanvas(300, 300);
-canvas.center();
-}
-
-function draw() {  
-}
-
-function Take_snapshot(){
-    save('myFilterImage.png');
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    tint_color = "";
+    }
+    
+    function draw(){
+        image(video, 0, 0, 640, 480);
+        tint(tint_color);
+        circle(35, 30, 50);
+        circle(605, 30, 50);
+        circle(35, 440, 50);
+        circle(605, 440, 50);
+        fill(255,0,0);
+        rect(60, 25, 520, 15);
+        rect(30, 55, 15, 360);
+        rect(600, 55, 15, 360);
+        rect(60, 430, 520, 15);
+        fill(0,128,0);
+        
+        
+    }
+    
+    function take_snapshot(){
+        save('student_name.png')
+    }
+    
+    function filter_tint(){
+        tint_color = document.getElementById("color_name").value;
     }
